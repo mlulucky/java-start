d10f4a7242b95304091f0ab8d35900b0a7f0d24c
mlulucky <dmswjdans@gmail.com>
2024-03-06 18:59:13 +0900

    OperationEx3

--- src/operator/ex/OperationEx3.java
+++ src/operator/ex/OperationEx3.java
@@ -5,6 +5,28 @@ public class OperationEx3 {
         int score = 101;
         boolean result = (80 <= score && score <=100);
         System.out.println(result);
+
+        Integer a = 100;
+        Integer b = 100;
+        System.out.println(a == b);
+
+        Integer c = 200;
+        Integer d = 200;
+        System.out.println(c == d);
+
+        String e = "100";
+        String f = "100";
+        System.out.println(e == f);
+
+        String g = "200";
+        String h = "200";
+        System.out.println(g == h);
+
+        String i = "hello";
+        String j = "hello";
+        System.out.println(i == j);
+
+
     }
 }
 
